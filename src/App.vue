b5efe435348ee6c4e0989335abade75654ff243d
Bismarck Momanyi <bismarquemomanyi@gmail.com>
2023-12-13 16:56:04 +0300

    fetch api id

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,21 @@ import Button from './components/Button.vue';
 import { ref } from 'vue';
 
 let adviceText = ref("It is easy to sit up and take notice, what's difficult is getting up and taking action.")
-const getRadom =()=>{
-  return Math.floor(Math.random()*100)
-}
+let adviceId = ref(177)
+const getRadom =()=>Math.floor(Math.random()*100)
 const getAdvice = async()=>{
   let id =getRadom();
   const res = await fetch(`https://api.adviceslip.com/advice/${id}`)
   const data = await res.json();
   adviceText.value = data.slip.advice;
-  // console.log(data)/;
+  adviceId = data.slip.id
 }
+console.log(adviceText.value);
 </script>
 
 <template>
   <div class="advice-co">
-    <Title no="177"/>
+    <Title :no="adviceId"/>
     <Body :advice="adviceText"/>
     <Divider/>
     <Button @getadvice="getAdvice" />
